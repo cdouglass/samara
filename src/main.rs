e4c32abc0cc2449820957117f96f5f8a77545c9e
Cathy Douglass <cdouglass256@gmail.com>
2017-08-09 17:08:55 -0700

    :help and :exit commands

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::io::Write;
 
 const GREETING : &str = "---- samara 0.1.0 --------------------------------------------------------------";
 const PROMPT : &str = "> ";
+const CMD_MARKER : char = ':';
+const USAGE : &str = "exit with :exit";
+
+enum Command {
+    Exit,
+    Help,
+    Unknown
+}
 
 fn main() {
     println!("{}", GREETING);
@@ -14,11 +22,32 @@ fn main() {
         let mut expr = String::new();
         io::stdin().read_line(&mut expr).unwrap();
 
-        let result = evaluate(&expr);
-        println!("{}", result);
+        let cmd = get_command(&expr);
+        match cmd {
+            Some(Command::Exit) => break,
+            Some(Command::Help) => println!("{}", USAGE),
+            Some(Command::Unknown) => println!("Unknown command"),
+            None => {
+                let result = evaluate(&expr);
+                println!("{}", result);
+            }
+        }
+
     }
 }
 
-fn evaluate(s: &str) -> String {
-    s.trim().chars().rev().collect()
+fn get_command(input: &str) -> Option<Command> {
+    if input.chars().nth(0) == Some(CMD_MARKER) {
+        match input.trim().trim_matches(CMD_MARKER) {
+            "exit" => Some(Command::Exit),
+            "help" => Some(Command::Help),
+            _ => Some(Command::Unknown)
+        }
+    } else {
+        None
+    }
+}
+
+fn evaluate(expr: &str) -> String {
+    expr.trim().chars().rev().collect()
 }
